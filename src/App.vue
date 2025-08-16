edbb031b83dee2ad9041fa471fc3fa0544eb5ccb
apriljavier <21-09454@g.batstate-u.edu.ph>
2025-08-16 10:59:09 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
+    <h1 class="text-5xl font-bold mb-4">Hi, I'm April (ノಠ益ಠ)ノ彡┻━┻ </h1>
+    <p class="text-xl text-gray-400 mb-6">A Computer Science Student & Aspiring Software Engineer</p>
+    <button class="px-6 py-3 bg-blue-600 rounded-xl hover:bg-blue-500 transition">
+      View My Work
+    </button>
+  </div>
+</template>
